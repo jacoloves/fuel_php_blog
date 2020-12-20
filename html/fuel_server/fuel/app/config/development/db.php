ad227eceb11708c52d4176ae7931a9a80455a026
jacoloves <5511068t@gmail.com>
2020-12-20 10:38:15 +0900

    2020-12-20

---
+++ html/fuel_server/fuel/app/config/development/db.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.2
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2019 Fuel Development Team
+ * @link       https://fuelphp.com
+ */
+
+/**
+ * -----------------------------------------------------------------------------
+ *  Database settings for development environment
+ * -----------------------------------------------------------------------------
+ *
+ *  These settings get merged with the global settings.
+ *
+ */
+
+return array(
+	'default' => array(
+		'connection' => array(
+			'dsn'      => 'mysql:host=mysql;dbname=fuel_blog',
+			'username' => 'root',
+      'password' => 'root',
+      'persistent' => false,
+      'compress' => false,
+    ),
+    'identifier' => '`',
+    'table_prefix' => '',
+    'charset' => 'utf8',
+    'enable_cache' => true,
+	),
+);
